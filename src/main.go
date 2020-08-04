b5c73f0ea7c3d3ba040f896e42bae6683fb22137
zhangxiaomin <zhangxiaomin@viewhigh.com>
2020-08-04 14:19:24 +0800

    db层代码优化 大漠孤烟直，长河落日圆

--- src/main.go
+++ src/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import ()
 
 func main() {
-	fmt.Println("meta-config")
+	/*client, _ := storage.Connect()
+	//user:= configservice.Userspace{"u123", "SuperStar", "read/write", nil, time.Now(),time.Now()}
+	//insert(client, user)
+	collection := client.Database("meta-config").Collection("user_space")
+	storage.Query(collection, bson.D{{"uid", "u123"}})*/
 }
--- src/storage/db.go
+++ src/storage/db.go
@@ -1,1 +1,70 @@
 package storage
+
+import (
+	"configservice"
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"time"
+)
+
+type mgo struct {
+	uri        string //数据库网络地址
+	database   string //要连接的数据库
+	collection string //要连接的集合
+}
+
+func (m *mgo) Connect() (*mongo.Collection, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(m.uri).SetMaxPoolSize(20))
+	if err != nil {
+		log.Print(err)
+	}
+	collection := client.Database(m.database).Collection(m.collection)
+	return collection, nil
+}
+
+/**
+ * 插入数据
+ */
+func (m *mgo) Insert(collection *mongo.Collection, user configservice.Userspace) (interface{}, error) {
+	insertResult, err := collection.InsertOne(context.TODO(), user)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	fmt.Println("Inserted a single document: ", insertResult.InsertedID)
+	return insertResult.InsertedID, nil
+}
+
+/**
+ * 查询数据
+ */
+func (m *mgo) Query(collection *mongo.Collection, filter bson.D) (interface{}, error) {
+	var result configservice.Userspace
+	err := collection.FindOne(context.TODO(), filter).Decode(&result)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	fmt.Printf("Found a single document: %+v\n", result)
+	return result, nil
+}
+
+/**
+ * 更新数据
+ */
+func (m *mgo) Update(collection *mongo.Collection, filter bson.D) (interface{}, error) {
+	deleteResult, err := collection.DeleteOne(context.TODO(), filter)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	fmt.Printf("Deleted %v documents in the trainers collection\n", deleteResult.DeletedCount)
+	return deleteResult, nil
+}
